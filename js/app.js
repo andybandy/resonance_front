c0e5dd556e84665984b46300f30f3a2486ab5add
Andrei Shchatko <andrei.shchatko@gmail.com>
2014-10-26 09:08:57 +0300

    Draws graphics

--- js/app.js
+++ js/app.js
@@ -11,37 +11,56 @@
     var sync = $firebase(ref);
     var self = this;
 
-    this.ds = [];
+    this.ds = {};
     this.data = sync.$asObject();
     ref.on('value', function(snap) {
       _.each(snap.val()._devices, function(d, ind) {
-        var unsortedSignals = _.map(d.signals, function(rate, time) {
-          return {time: parseInt(time), rate: rate};
-        });
-        self.ds[ind] = [];
-        var signals = _.sortBy(unsortedSignals, function(n) {
-          return n.time;
-        });
-        signals = signals.slice(Math.max(signals.length - steps, 1));
-        var min = signals[0].time;
-        var max = signals[signals.length - 1].time;
-        var step = (max - min)/steps;
-        var c = 0;
-        for (var i = 0; i < steps; i++) {
-          while (signals[0].time + i*step > signals[c + 1].time) {
-            c++;
+        if (d.signals) {
+          var unsortedSignals = _.map(d.signals, function(rate, time) {
+            return {time: parseInt(time), rate: rate};
+          });
+          self.ds[ind] = [];
+          var signals = _.sortBy(unsortedSignals, function(n) {
+            return n.time;
+          });
+          signals = signals.slice(Math.max(signals.length - steps, 1));
+          var min = signals[0].time;
+          var max = signals[signals.length - 1].time;
+          var step = (max - min)/steps;
+          var c = 0;
+          for (var i = 0; i < steps; i++) {
+            while (signals[0].time + i*step > signals[c + 1].time) {
+              c++;
+            }
+            var value = interpolate(signals[c], signals[c + 1], i*step + signals[0].time);
+            self.ds[ind].push({time: i, rate: value});
           }
-          var value = interpolate(signals[c], signals[c + 1], i*step + signals[0].time);
-          self.ds[ind].push({time: i, rate: value});
+          console.log(JSON.stringify(self.ds[ind]));
+          console.log(signals.length);
         }
-        console.log(JSON.stringify(self.ds[ind]));
-        console.log(signals.length);
-        /*
-        var canvas = $('#canvas' + ind);
-        var context = canvas.getContext('2d');
-        */
       });
     });
+
+    setInterval(function() {
+      var _counter = 0;
+      _.each(self.ds, function(d, ind) {
+        var canvas = $('#canvas' + _counter);
+        _counter++;
+        if (canvas.length > 0) {
+          var context = canvas[0].getContext('2d');
+          context.clearRect(0, 0, 450, 200);
+          context.beginPath();
+          context.lineWidth = 5;
+          context.strokeStyle = 'blue';
+          context.moveTo(0, 200 - d[0].rate);
+          _.each(d, function(point) {
+            context.lineTo(point.time*9, 200 - point.rate);
+          });
+          context.stroke();
+          context.closePath();
+        }
+      });
+    }, 1000);
   }]);
 
   app.directive('devices', function($templateCache) {
